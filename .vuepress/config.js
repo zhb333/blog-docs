9be9100ab91c2994c5baff7614ebd5ae3b0acd32
zhb333 <1140457303@qq.com>
2020-03-22 02:21:02 +0800

    开发完成

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  base: '/readme-blog/',
   title: 'forapi.cn', // Title for the site. This will be displayed in the navbar.
   description: '学习记录 代码分享 经验总结 开发教程',
   lange: 'zh-CN',
@@ -49,6 +50,30 @@ module.exports = {
           link: 'https://github.com/zhb333/readme-blog',
         },
       ]
-    }
+    },
+    comment: {
+      service: 'vssue',
+      owner: 'zhb333',
+      repo: 'readme-blog', 
+      clientId: '',
+      clientSecret: '',
+    },
+
+    newsletter: {
+      title: '邮件订阅',
+      content: '请输入您的邮箱...',
+      submitText: '订阅',
+      endpoint: 'https://forapi.us19.list-manage.com/subscribe/post?u=ac82b3046426d14cad8f2a3ef&amp;id=ebfc431c44'
+    },
+
+    sitemap: {
+      hostname: 'http://forapi.cn'
+    },
+
+    feed: {
+      canonical_base:'http://forapi.cn',
+    },
+
+    smoothScroll: true
   }
 }
