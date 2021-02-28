6afc85b714cdf7b5f3b9b4d67536390a7d32f132
zhb333 <1140457303@qq.com>
2021-02-28 18:16:47 +0800

    代码上传

---
+++ .vuepress/config.js
@@ -0,0 +1,89 @@
+module.exports = {
+  "title": "Forapi.cn",
+  "description": "虽千万里，吾往矣！",
+  "dest": "dist",
+  "head": [
+    [
+      "link",
+      {
+        "rel": "icon",
+        "href": "/favicon.ico"
+      }
+    ],
+    [
+      "meta",
+      {
+        "name": "viewport",
+        "content": "width=device-width,initial-scale=1,user-scalable=no"
+      }
+    ]
+  ],
+  // "theme": "reco",
+  "themeConfig": {
+    "nav": [
+      {
+        "text": "首页",
+        "link": "/",
+        "icon": "reco-home"
+      },
+      {
+        "text": "时间轴",
+        "link": "/timeline/",
+        "icon": "reco-date"
+      },
+      {
+        "text": "联系",
+        "icon": "reco-message",
+        "items": [
+          {
+            "text": "GitHub",
+            "link": "https://github.com/recoluan",
+            "icon": "reco-github"
+          }
+        ]
+      }
+    ],
+    "type": "blog",
+    "blogConfig": {
+      "category": {
+        "location": 2,
+        "text": "分类"
+      },
+      "tag": {
+        "location": 3,
+        "text": "标签"
+      }
+    },
+    "friendLink": [
+      {
+        "title": "游戏管理后台",
+        "desc": "纯前端的游戏管理后台系统，后端数据为模拟数据",
+        "email": "1140454303@qq.com",
+        "link": "https://zhanghuanbiao.gitee.io/vv-admin"
+      },
+      {
+        "title": "专题编辑器",
+        "desc": "机顶盒专题拖拽编辑器",
+        "email": "1140454303@qq.com",
+        "link": "https://zhanghuanbiao.gitee.io/topic-editor/"
+      },
+      {
+        "title": "tv.js文档",
+        "desc": "tv.js 纯es3规范，编写的机顶盒 JavaScript 运行库",
+        "email": "1140454303@qq.com",
+        "link": "https://zhanghuanbiao.gitee.io/tv-docs/"
+      }
+    ],
+    "logo": "/logo.png",
+    "search": true,
+    "searchMaxSuggestions": 10,
+    "lastUpdated": "Last Updated",
+    "author": "zhb333",
+    "authorAvatar": "/avatar.jpeg",
+    "record": "xxxx",
+    "startYear": "2017",
+  },
+  "markdown": {
+    "lineNumbers": true
+  }
+}
---
+++ .vuepress/theme/components/Decor.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="decor" style="position: relative">
+    <svg
+      id="header-decor"
+      class="decor bottom"
+      xmlns="http://www.w3.org/2000/svg"
+      version="1.1"
+      viewBox="0 0 100 100"
+      preserveAspectRatio="none"
+    >
+      <path
+        class="large left"
+        d="M0 0 L50 50 L0 100"
+        fill="rgba(255,255,255, .1)"
+      ></path>
+      <path
+        class="large right"
+        d="M100 0 L50 50 L100 100"
+        fill="rgba(255,255,255, .1)"
+      ></path>
+      <path
+        class="medium left"
+        d="M0 100 L50 50 L0 33.3"
+        fill="rgba(255,255,255, .3)"
+      ></path>
+      <path
+        class="medium right"
+        d="M100 100 L50 50 L100 33.3"
+        fill="rgba(255,255,255, .3)"
+      ></path>
+      <path
+        class="small left"
+        d="M0 100 L50 50 L0 66.6"
+        fill="rgba(255,255,255, .5)"
+      ></path>
+      <path
+        class="small right"
+        d="M100 100 L50 50 L100 66.6"
+        fill="rgba(255,255,255, .5)"
+      ></path>
+      <path
+        d="M0 99.9 L50 49.9 L100 99.9 L0 99.9"
+        fill="rgba(255,255,255, 1)"
+      ></path>
+      <path d="M48 52 L50 49 L52 52 L48 52" fill="rgba(255,255,255, 1)"></path>
+    </svg>
+    <div
+      style="
+        height: 2px;
+        width: 100%;
+        bottom: -1px;
+        position: absolute;
+        background: #fff;
+      "
+    ></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Decor",
+};
+</script>
+
+<style scoped>
+.decor {
+  max-height: 200px;
+}
+</style>
